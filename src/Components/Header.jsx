fe5ec5445407c4e22da78e9f6d0dfc406fbd33be
SuperMarios <110827896+supermariya@users.noreply.github.com>
2023-08-07 14:28:11 +0300

    Add Header Styling And Code

---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <div className="Header">
+            <header>
+                <div className="logo">
+                    <p>Mariya Baig</p>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Projects</a></li>
+                        <li><a href="/">Skills</a></li>
+                    </ul>
+                </nav>
+                <button className="cta"><a href="/">Hire Me</a></button>
+            </header>
+        </div>
+    );
+
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
+import App from "./Components/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 
